16d899e4fecf0b8a7ef5ab9bac71b016533dd030
maaqoul <hicham.maaqoul@gmail.com>
2021-03-01 12:04:29 +0100

    add passengers data

---
+++ src/assets/passengers.ts
@@ -0,0 +1,32 @@
+passengers: any = [
+  {
+    id: 1,
+    fullName: "Stephen",
+    checkedIn: true,
+    checkInDate: 1490742000000,
+  },
+  {
+    id: 2,
+    fullName: "Rose",
+    checkedIn: false,
+    checkInDate: null,
+  },
+  {
+    id: 3,
+    fullName: "James",
+    checkedIn: true,
+    checkInDate: 1491606000000,
+  },
+  {
+    id: 4,
+    fullName: "Louise",
+    checkedIn: true,
+    checkInDate: 1488412800000,
+  },
+  {
+    id: 5,
+    fullName: "Tina",
+    checkedIn: false,
+    checkInDate: null,
+  },
+];
